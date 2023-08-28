4c3dee93cdb0038869785aec4b924e123729e780
deepak-543 <53090546+deepak-543@users.noreply.github.com>
2023-08-28 13:13:01 +0530

    Update with new new app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express(); // Initializing Express App
 // Sending Hello World when anyone browse your webpage
 
 app.get("/*", (req, res)=>{
-        res.send('Hello World Devops Test New'); 
+        res.send('Hello World Devops Test New 2'); 
 });
 
 app.listen(3000, ()=> console.log("App Listening on port 3000"));
